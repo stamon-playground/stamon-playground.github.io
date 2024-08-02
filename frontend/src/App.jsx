30f4831127b2c91210eb57db0e1ca221754f9282
瞿相荣 <woshiquxiangrong@outlook.com>
2024-08-02 17:17:28 +0800

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -55,11 +55,11 @@ languages["stamon"] = {
       alias: 'language'
     },
     {
-      pattern: /\b\d+\b/,
+      pattern: /\b([0-9]+)\b/,
       alias: 'numeric'
     },
     {
-      pattern: /\b\d+\.\d+\b/,
+      pattern: /\b([0-9]+)(\.)([0-9]+)\b/,
       alias: 'numeric'
     }
   ],
@@ -110,26 +110,21 @@ import { languageMap } from "solid-prism-editor"
 languageMap.stamon = {
   comments: {
     line: "//",
-    block: ["/*", "*/"]
-  },
-  getComments(editor, position) {
-    // Method called when a user executes a comment toggling command
-    // Useful if a language uses different comment tokens in different contexts
-    // Currently used by JSX so {/* */} is used to toggle comments in JSX contexts
   },
   autoIndent: [
-    // Whether to indent
     ([start], value) => /[([{][^\n)\]}]*$/.test(value.slice(0, start)),
-    // Whether to add an extra line
     ([start, end], value) => /\[]|\(\)|{}/.test(value[start - 1] + value[end])
   ],
-  autoCloseTags([start, end, direction], value) {
-    // Function called when the user types ">", intended to auto close tags.
-    // If a string is returned, it will get inserted behind the cursor.
+  autoCloseTags([start, _end, _direction], value) {
     let match = /<(\w+)\b.*>/.exec(getLineBefore(value, start) + ">")
 		return match && `</${match[1]}>`
   },
 }
+
+const exampleCode = `// Stamon example
+import std;
+print("Hello world!");
+`
 
 export default () => {
   const [input, setInput] = createSignal("");
@@ -152,28 +147,68 @@ export default () => {
   };
 
   return (
-    <Box sx={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
+    <Box sx={{ display: 'flex', flexDirection: 'column', height: '100vh', paddingBottom: "1em"}}>
       <CssBaseline />
       <AppBar position="sticky" color="inherit" elevation={3}>
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Stamon Playground
           </Typography>
-          <Button color="inherit" onClick={handleInput}>Run</Button>
+          <Button color="inherit" variant="outlined" onClick={handleInput}>Run</Button>
         </Toolbar>
       </AppBar>
-      <Box sx={{ flex: 1, display: 'flex' }}>
-        <Grid container spacing={2} sx={{ flex: 1 }}>
-          <Grid item xs={12} md={7} sx={{ display: 'flex', flexDirection: 'column' }}>
-            <Editor language="stamon" tabSize={4} style={{ flex: 1 }} extensions={basicSetup} onUpdate={setInput} />
-          </Grid>
-          <Grid item xs={12} md={5} sx={{ display: 'flex', flexDirection: 'column' }}>
-            <Paper variant="outlined" sx={{ flex: 1 }}>
-              <Editor readOnly style={{ flex: 1 }} value={output()} lineNumbers={false} />
-            </Paper>
-          </Grid>
-        </Grid>
+      <Box sx={{ display: 'grid', gridTemplateColumns: { xs: '1fr 16px 1fr' }, height: '100%', minHeight: 0 }}>
+        <Editor
+          language="stamon"
+          tabSize={4}
+          extensions={basicSetup}
+          onUpdate={setInput}
+          value={exampleCode}
+          style={{ height: '100%' }}
+        />
+        <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+          <Typography variant="h6">::</Typography>
+        </Box>
+          <Editor
+            readOnly
+            value={output()}
+            style={{ height: '100%' }}
+            lineNumbers={false}
+          />
       </Box>
     </Box>
   );
+
+  // return (
+  //   <Box sx={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
+  //     <CssBaseline />
+  //     <AppBar position="sticky" color="inherit" elevation={3}>
+  //       <Toolbar>
+  //         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+  //           Stamon Playground
+  //         </Typography>
+  //         <Button color="inherit" variant="outlined" onClick={handleInput}>Run</Button>
+  //       </Toolbar>
+  //     </AppBar>
+  //     <Box sx={{ height: '100%' }}>
+  //       <Grid container spacing={2} sx={{ flex: 1 }}>
+  //         <Grid item xs={12} md={7} sx={{ display: 'flex', flexDirection: 'column' }}>
+  //           <Editor
+  //             language="stamon"
+  //             tabSize={4}
+  //             style={{ flex: 1 }}
+  //             extensions={basicSetup}
+  //             onUpdate={setInput}
+  //             value={exampleCode}
+  //           />
+  //         </Grid>
+  //         <Grid item xs={12} md={5} sx={{ display: 'flex', flexDirection: 'column' }}>
+  //           <Paper variant="outlined" sx={{ flex: 1 }}>
+  //             <Editor readOnly style={{ flex: 1 }} value={output()} lineNumbers={false} />
+  //           </Paper>
+  //         </Grid>
+  //       </Grid>
+  //     </Box>
+  //   </Box>
+  // );
 }
